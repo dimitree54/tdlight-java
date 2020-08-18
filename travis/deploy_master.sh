4c815e3786899bba3201d5c45bef9516971a9df0
Andrea Cavalli <andrea@cavallium.it>
2020-08-18 17:27:52 +0200

    Update .travis.yml, pom.xml, and deploy_master.sh

--- travis/deploy_master.sh
+++ travis/deploy_master.sh
@@ -1,5 +1,21 @@
 #!/bin/bash -e
-echo "Deployed"
-echo "$TRAVIS_OS_NAME"
+if [ "$TRAVIS_CPU_ARCH" = "arm64" ]; then
+    export TRAVIS_CPU_ARCH_STANDARD="aarch64"
+else
+    export TRAVIS_CPU_ARCH_STANDARD="${TRAVIS_CPU_ARCH,,}"
+fi
+export TRAVIS_OS_NAME_STANDARD="${TRAVIS_OS_NAME,,}"
+
+echo "TRAVIS_OS_NAME: $TRAVIS_OS_NAME"
+echo "TRAVIS_OS_NAME_STANDARD: $TRAVIS_OS_NAME_STANDARD"
+echo "TRAVIS_CPU_ARCH: $TRAVIS_CPU_ARCH"
+echo "TRAVIS_CPU_ARCH_STANDARD: $TRAVIS_CPU_ARCH_STANDARD"
+
+echo "$GIT_IGN_TRAVIS_DEPLOY_PRIVATE_KEY" > ~/.ssh/id_rsa
+
+git clone "git@ssh.git.ignuranza.net:tdlight-team/tdlight-java-natives-$TRAVIS_OS_NAME_STANDARD-$TRAVIS_CPU_ARCH_STANDARD.git"
+cd "tdlight-java-natives-$TRAVIS_OS_NAME_STANDARD-$TRAVIS_CPU_ARCH_STANDARD"
+ls -alch
+
 ls -alch $TRAVIS_BUILD_DIR/out
 ls -alch $TRAVIS_BUILD_DIR
