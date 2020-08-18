5126d723b04d5e0726116e1ac65131a697d91a9d
Andrea Cavalli <andrea@cavallium.it>
2020-08-18 22:14:09 +0200

    Update .travis.yml and linux.sh

--- travis/linux.sh
+++ travis/linux.sh
@@ -3,8 +3,9 @@
 mkdir $TRAVIS_BUILD_DIR/out
 cd src/main/jni
 
-export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-$(dpkg --print-architecture)"
-export PATH="$PATH:/usr/lib/jvm/java-11-openjdk-$(dpkg --print-architecture)/bin"
+#export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-$(dpkg --print-architecture)"
+#export PATH="$PATH:/usr/lib/jvm/java-11-openjdk-$(dpkg --print-architecture)/bin"
+sudo apt install openjdk-11-jdk-headless
 
 export TD_SRC_DIR=${PWD}/td
 export TD_BIN_DIR=${PWD}/jtdlib/td
@@ -20,14 +21,14 @@ cd jnibuild
 #export JAVA_HOME=/usr/lib/jvm/java-1.13.0-openjdk-amd64
 #export JAVA_INCLUDE_PATH=/usr/lib/jvm/java-1.13.0-openjdk-amd64/include/
 cmake -DCMAKE_BUILD_TYPE=Release -DTD_ENABLE_JNI=ON -DCMAKE_INSTALL_PREFIX:PATH=${TD_BIN_DIR} ${TD_SRC_DIR}
-cmake --build . --target install -- -j4
+cmake --build . --target install -- -j3
 
 cd ../../../../../
 #mvn install -X
 
 cd src/main/jni/jtdlib/build
 cmake -DCMAKE_BUILD_TYPE=Release -DTd_DIR=${TD_BIN_DIR}/lib/cmake/Td -DJAVA_SRC_DIR=${JAVA_SRC_DIR} -DCMAKE_INSTALL_PREFIX:PATH=.. ..
-cmake --build . --target install -- -j4
+cmake --build . --target install -- -j3
 cd ..
 #rm -r jnibuild
 #rm -r build
