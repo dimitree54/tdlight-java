5557cea9e28c77174a4ce0f22086e946b1bdef47
Andrea Cavalli <andrea@cavallium.it>
2020-08-19 23:41:17 +0200

    Update build.sh

--- travis/build.sh
+++ travis/build.sh
@@ -9,10 +9,11 @@ source ./travis/setup_variables.sh
 cd $TD_BUILD_DIR
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   cmake -DCMAKE_BUILD_TYPE=Release -DTD_ENABLE_JNI=ON -DCMAKE_INSTALL_PREFIX:PATH=${TD_BIN_DIR} ${TD_SRC_DIR}
+  cmake --build $TD_BUILD_DIR --target prepare_cross_compiling -- -j4
 elif [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
   cmake -A x64 -DCMAKE_BUILD_TYPE=Release -DTD_ENABLE_JNI=ON -DCMAKE_INSTALL_PREFIX:PATH=${TD_BIN_DIR} -DCMAKE_TOOLCHAIN_FILE:FILEPATH=$VCPKG_DIR/scripts/buildsystems/vcpkg.cmake ${TD_SRC_DIR}
+  cmake --build $TD_BUILD_DIR --target prepare_cross_compiling -- -m
 fi
-cmake --build $TD_BUILD_DIR --target prepare_cross_compiling
 
 # Split sources
 cd $TD_SRC_DIR
@@ -21,13 +22,13 @@ php SplitSource.php
 # Build
 cd $TD_BUILD_DIR
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  cmake --build $TD_BUILD_DIR --target tdjson -- -j2
-  cmake --build $TD_BUILD_DIR --target tdjson_static -- -j2
+  #cmake --build $TD_BUILD_DIR --target tdjson -- -j2
+  #cmake --build $TD_BUILD_DIR --target tdjson_static -- -j2
   cmake --build $TD_BUILD_DIR --target install --config Release -- -j2
 elif [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
-  cmake --build $TD_BUILD_DIR --target tdjson
-  cmake --build $TD_BUILD_DIR --target tdjson_static
-  cmake --build $TD_BUILD_DIR --target install --config Release
+  #cmake --build $TD_BUILD_DIR --target tdjson -- -m
+  #cmake --build $TD_BUILD_DIR --target tdjson_static -- -m
+  cmake --build $TD_BUILD_DIR --target install --config Release -- -m
 fi
 
 # After build
@@ -41,5 +42,5 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   cmake --build $TDNATIVES_CPP_BUILD_DIR --target install -- -j2
 elif [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
   cmake -A x64 -DCMAKE_BUILD_TYPE=Release -DTD_BIN_DIR=${TD_BIN_DIR} -DTDNATIVES_BIN_DIR=${TDNATIVES_BIN_DIR} -DTDNATIVES_DOCS_BIN_DIR=${TDNATIVES_DOCS_BIN_DIR} -DTd_DIR=${TD_BIN_DIR}/lib/cmake/Td -DJAVA_SRC_DIR=${JAVA_SRC_DIR} -DTDNATIVES_CPP_SRC_DIR:PATH=$TDNATIVES_CPP_SRC_DIR -DCMAKE_TOOLCHAIN_FILE:FILEPATH=$VCPKG_DIR/scripts/buildsystems/vcpkg.cmake $TDNATIVES_CPP_SRC_DIR
-  cmake --build $TDNATIVES_CPP_BUILD_DIR --target install --config Release
+  cmake --build $TDNATIVES_CPP_BUILD_DIR --target install --config Release -- -m
 fi
