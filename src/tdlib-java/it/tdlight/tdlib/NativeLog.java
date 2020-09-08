cd4013756b16a7b3dc02c41d6421235f92f2aec3
Andrea Cavalli <andrea@cavallium.it>
2020-09-08 16:25:31 +0200

    Fix fatal error callback

--- src/tdlib-java/it/tdlight/tdlib/NativeLog.java
+++ src/tdlib-java/it/tdlight/tdlib/NativeLog.java
@@ -18,26 +18,68 @@
 package it.tdlight.tdlib;
 
 /**
- * Interface for managing the internal logging of TDLib. By default TDLib writes logs to stderr or an OS specific log and uses a verbosity level of 5.
+ * Class used for managing internal TDLib logging.
+ * Use TdApi.*Log* methods instead.
  */
 public class NativeLog {
 
+    private static final FatalErrorCallbackPtr defaultFatalErrorCallbackPtr = System.err::println;
+    private static FatalErrorCallbackPtr fatalErrorCallback = defaultFatalErrorCallbackPtr;
+
     /**
-     * Sets the path to the file to where the internal TDLib log will be written. By default TDLib writes logs to stderr or an OS specific log. Use this method to write the log to a file instead.
-     * @param filePath Path to a file where the internal TDLib log will be written. Use an empty path to switch back to the default logging behaviour.
-     * @return True on success, or false otherwise, i.e. if the file can't be opened for writing.
+     * Sets file path for writing TDLib internal log. By default TDLib writes logs to the System.err.
+     * Use this method to write the log to a file instead.
+     *
+     * @deprecated As of TDLib 1.4.0 in favor of {@link TdApi.SetLogStream}, to be removed in the future.
+     * @param filePath Path to a file for writing TDLib internal log. Use an empty path to
+     *                 switch back to logging to the System.err.
+     * @return whether opening the log file succeeded.
      */
+    @Deprecated
     public static native boolean setFilePath(String filePath);
 
     /**
-     * Sets maximum size of the file to where the internal TDLib log is written before the file will be auto-rotated. Unused if log is not written to a file. Defaults to 10 MB.
-     * @param maxFileSize Maximum size of the file to where the internal TDLib log is written before the file will be auto-rotated. Should be positive.
+     * Changes the maximum size of TDLib log file.
+     *
+     * @deprecated As of TDLib 1.4.0 in favor of {@link TdApi.SetLogStream}, to be removed in the future.
+     * @param maxFileSize The maximum size of the file to where the internal TDLib log is written
+     *                    before the file will be auto-rotated. Must be positive. Defaults to 10 MB.
      */
+    @Deprecated
     public static native void setMaxFileSize(long maxFileSize);
 
     /**
-     * Sets the verbosity level of the internal logging of TDLib. By default the TDLib uses a verbosity level of 5 for logging.
-     * @param verbosityLevel New value of the verbosity level for logging. Value 0 corresponds to fatal errors, value 1 corresponds to errors, value 2 corresponds to warnings and debug warnings, value 3 corresponds to informational, value 4 corresponds to debug, value 5 corresponds to verbose debug, value greater than 5 and up to 1024 can be used to enable even more logging.
+     * Changes TDLib log verbosity.
+     *
+     * @deprecated As of TDLib 1.4.0 in favor of {@link TdApi.SetLogVerbosityLevel}, to be removed in the future.
+     * @param verbosityLevel New value of log verbosity level. Must be non-negative.
+     *                       Value 0 corresponds to fatal errors,
+     *                       value 1 corresponds to java.util.logging.Level.SEVERE,
+     *                       value 2 corresponds to java.util.logging.Level.WARNING,
+     *                       value 3 corresponds to java.util.logging.Level.INFO,
+     *                       value 4 corresponds to java.util.logging.Level.FINE,
+     *                       value 5 corresponds to java.util.logging.Level.FINER,
+     *                       value greater than 5 can be used to enable even more logging.
+     *                       Default value of the log verbosity level is 5.
      */
+    @Deprecated
     public static native void setVerbosityLevel(int verbosityLevel);
+
+    /**
+     * This function is called from the JNI when a fatal error happens to provide a better error message.
+     * The function does not return.
+     *
+     * @param errorMessage Error message.
+     */
+    private static synchronized void onFatalError(String errorMessage) {
+        new Thread(() -> NativeLog.fatalErrorCallback.onFatalError(errorMessage)).start();
+    }
+
+    /**
+     * Sets the callback that will be called when a fatal error happens. None of the TDLib methods can be called from the callback. The TDLib will crash as soon as callback returns. By default the callback set to print in stderr.
+     * @param fatalErrorCallback Callback that will be called when a fatal error happens. Pass null to restore default callback.
+     */
+    public static synchronized void setFatalErrorCallback(FatalErrorCallbackPtr fatalErrorCallback) {
+        NativeLog.fatalErrorCallback = ObjectsUtils.requireNonNullElse(fatalErrorCallback, defaultFatalErrorCallbackPtr);
+    }
 }
